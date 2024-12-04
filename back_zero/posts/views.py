edb8bcc7195c470e09ba5d4af710d703dc8fb5d1
sas0mir <ssmirnov@i-novus.ru>
2024-12-04 23:45:40 +0100

    L-008: conditions for nav elements viewing + newPost page

--- back_zero/posts/views.py
+++ back_zero/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def posts_list(request):
@@ -9,3 +10,7 @@ def posts_list(request):
 def posts_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', { 'post': post })
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html') 
